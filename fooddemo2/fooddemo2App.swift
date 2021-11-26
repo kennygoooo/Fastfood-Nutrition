8cea9e7e38186f1c6a027fedcfbbb2f0c2c7c677
Kennt <keny7946@gmail.com>
2021-11-26 12:25:56 +0800

    Initial Commit

---
+++ fooddemo2/fooddemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  fooddemo2App.swift
+//  fooddemo2
+//
+//  Created by kenny on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct fooddemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
